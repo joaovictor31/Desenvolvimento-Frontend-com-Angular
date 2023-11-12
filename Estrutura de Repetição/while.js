d548fc2c21633dca5ab09fd471a1d5c9c9dde778
João Victor <101882992+joaovictor31@users.noreply.github.com>
2023-11-12 16:22:12 -0300

    d

--- Estrutura de Repetição/while.js
+++
@@ -1,9 +0,0 @@
-let lista = [10,30,10,50];
-let soma = 0;
-let contador = 0;
-while(contador < lista.length){
-    soma+= lista[contador];
-    contador++
-    
-}console.log(soma);
-
---
+++ funcao02.js
@@ -0,0 +1,4 @@
+function soma(a,b){
+    let valor = a + b;
+    console.log(valor);
+}
